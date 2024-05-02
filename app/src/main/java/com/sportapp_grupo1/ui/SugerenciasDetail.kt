6ad12489dae5c72753fdc810a33024f9984ee115
shiomar.salazar-at-312883711942 <s.salazarc@uniandes.edu.co>
2024-05-02 15:26:42 -0600

    Se agregan cambios iniciales UI

---
+++ app/src/main/java/com/sportapp_grupo1/ui/SugerenciasDetail.kt
@@ -0,0 +1,28 @@
+package com.sportapp_grupo1.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.sportapp_grupo1.R
+
+
+class SugerenciasDetail : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.sugerencias_detail_fragment, container, false)
+    }
+
+
+}
