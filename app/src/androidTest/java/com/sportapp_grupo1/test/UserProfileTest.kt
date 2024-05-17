742e77561adbfb06fbf849f1c0a10054bef18b9e
shiomar1108 <s.salazarc@uniandes.edu.co>
2024-05-16 19:40:18 -0600

    Update UserProfileTest.kt

--- app/src/androidTest/java/com/sportapp_grupo1/test/UserProfileTest.kt
+++ app/src/androidTest/java/com/sportapp_grupo1/test/UserProfileTest.kt
@@ -27,7 +27,7 @@ class UserProfileTest {
         ActivityScenarioRule(MainActivity::class.java)
 
     //Constante que define el tiempo de espera para que se carguen los datos retornados por el adapter
-    val delayService2 = Integer.toUnsignedLong(8500)
+    val delayService2 = Integer.toUnsignedLong(10000)
 
     fun clickIntoButtonById(idView: Int) {
         //Damos click en el boton idView
